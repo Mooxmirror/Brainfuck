3fef72aaa062b2787a1364ebb8c156246901c9f7
Mooxmirror <github@mooxmirror.de>
2015-02-03 21:00:32 +0100

    First commit
    A small brainfuck interpreter soon available on bf.mooxmirror.io

---
+++ interpreter.js
@@ -0,0 +1,89 @@
+/*
+
+A small Brainfuck interpreter written in JavaScript.
+(c) 2015 mooxmirror
+
+*/
+
+function interpret(code, input) {
+    var currentIndex = 0,
+        currentChar = '',
+        inputIndex = 0,
+        dataStorage = [0],
+        dataIndex = 0,
+        output = '';
+
+    var getLoopStart = function(code, current) {
+        var lvl = 0,
+            currentChar = '';
+        for (var i = current - 1; i >= 0; i--) {
+            currentChar = code.charAt(i);
+            if (lvl == 0 && currentChar == '[')
+                return i;
+            else {
+                if (currentChar == '[')
+                    lvl--;
+                if (currentChar == ']')
+                    lvl++;
+            }
+        }
+        return -1;
+    };
+    var getLoopEnd = function(code, current) {
+        var lvl = 0,
+            currentChar = '';
+        for (var i = current + 1; i < code.length; i++) {
+            currentChar = code.charAt(i);
+            if (lvl == 0 && currentChar == ']')
+                return i;
+            else {
+                if (currentChar == ']')
+                    lvl--;
+                if (currentChar == '[')
+                    lvl++;
+            }
+        }
+        return -1;
+    };
+
+    code = code.replace(/[^<>,\.\[\]\+-]/g, '');
+
+    for (currentIndex = 0; currentIndex < code.length; currentIndex++) {
+        currentChar = code.charAt(currentIndex);
+
+        switch (currentChar) {
+            case '+':
+                dataStorage[dataIndex] ++;
+                break;
+            case '-':
+                dataStorage[dataIndex] --;
+                break;
+            case '>':
+                dataIndex++;
+                if (dataIndex == dataStorage.length)
+                    dataStorage.push(0);
+                break;
+            case '<':
+                dataIndex--;
+                break;
+            case '.':
+                output += String.fromCharCode(dataStorage[dataIndex]);
+                break;
+            case ',':
+                if (inputIndex == input.length)
+                    dataStorage[dataIndex] = 0;
+                else
+                    dataStorage[dataIndex] = input.charCodeAt(inputIndex++);
+                break;
+            case '[':
+                if (dataStorage[dataIndex] == 0)
+                    currentIndex = getLoopEnd(code, currentIndex);
+                break;
+            case ']':
+                if (dataStorage[dataIndex] != 0)
+                    currentIndex = getLoopStart(code, currentIndex);
+                break;
+        }
+    }
+    return output;
+}
